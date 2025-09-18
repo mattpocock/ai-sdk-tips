ccadfd3139994eff18501c04811a0170e289acb6
Matt Pocock <mattpocockvoice@gmail.com>
2025-09-18 14:10:52 +0100

    Updates

--- exercises/02-fundamentals/02.02-structured-outputs/explainer/main.ts
+++ exercises/02-fundamentals/02.02-structured-outputs/explainer/main.ts
@@ -1,42 +1,55 @@
 import { anthropic } from '@ai-sdk/anthropic';
-import { generateObject, streamText } from 'ai';
+import {
+  generateObject,
+  generateText,
+  streamText,
+} from 'ai';
 import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import z from 'zod';
 
 const invoice = readFileSync(
-  path.join(import.meta.dirname, 'invoice.pdf'),
+  path.join(
+    import.meta.dirname,
+    'invoice.pdf',
+  ),
 );
 
-const result = await generateObject({
-  schema: z.object({
-    items: z.array(
-      z.object({
-        name: z.string(),
-        quantity: z.number(),
-        price: z.number(),
-      }),
+const result =
+  await generateObject({
+    schema: z.object({
+      items: z.array(
+        z.object({
+          name: z.string(),
+          quantity: z.number(),
+          price: z.number(),
+        }),
+      ),
+      total: z.number(),
+      currency: z.string(),
+    }),
+    model: anthropic(
+      'claude-3-7-sonnet-20250219',
     ),
-    total: z.number(),
-    currency: z.string(),
-  }),
-  model: anthropic('claude-3-7-sonnet-20250219'),
-  messages: [
-    {
-      role: 'user',
-      content: [
-        {
-          type: 'text',
-          text: 'Give me a summary of this invoice.',
-        },
-        {
-          type: 'file',
-          data: invoice,
-          mediaType: 'application/pdf',
-        },
-      ],
-    },
-  ],
-});
+    messages: [
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: 'Give me a summary of this invoice.',
+          },
+          {
+            type: 'file',
+            data: invoice,
+            mediaType:
+              'application/pdf',
+          },
+        ],
+      },
+    ],
+  });
 
-console.dir(result.object, { depth: null });
+console.dir(result.object, {
+  depth: null,
+});
