5ac2e04a92a48b6932ca7097e89ca52c9c6835af
Matt Pocock <mattpocockvoice@gmail.com>
2025-09-23 09:44:47 +0100

    Added formatting data efficiently tip

---
+++ exercises/03-tokens/03.05-formatting-data-efficiently/explainer/main.ts
@@ -0,0 +1,36 @@
+import { readFileSync } from 'node:fs';
+import { Tiktoken } from 'js-tiktoken/lite';
+import o200k_base from 'js-tiktoken/ranks/o200k_base';
+import path from 'node:path';
+
+const tokenizer = new Tiktoken(o200k_base);
+
+const jsonData = readFileSync(
+  path.join(import.meta.dirname, 'data.json'),
+  'utf-8',
+);
+
+const jsonTokens = tokenizer.encode(jsonData);
+
+console.log(jsonTokens.length);
+
+const formattedData = JSON.parse(jsonData)
+  .map(
+    (item: {
+      text: string;
+      start: number;
+      end: number;
+      speaker: string;
+      confidence: number;
+    }) => {
+      return `${item.speaker}|${item.confidence}|${item.text}|${item.start}|${item.end}`;
+    },
+  )
+  .join('\n');
+
+const headerLine = 'speaker|confidence|text|start|end';
+const formattedTokens = tokenizer.encode(
+  headerLine + '\n' + formattedData,
+);
+
+console.log(formattedTokens.length);
